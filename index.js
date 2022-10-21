0e9d58a8f05f06592e5317497a863801cc4bda1f
echubi <echubiojo@gmail.com>
2022-10-21 17:45:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://localhost:27017/message-database'
 const port = process.env.PORT || 5000;
 
+const Msg = require('./chatSchema')
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+mongoose.connect(mongoDB).then(() => {
+    console.log('connected' )
+}).catch(err => console.log(err));
+
 io.on('connection', (socket) => {
+    Msg.find().then(result => {
+        socket.emit('output-messages', result)
+    })
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
     socket.on('chat message', msg => {
-        io.emit('chat message', msg);
+        const message = new Msg({message:msg}) 
+        message.save().then(() => {
+            io.emit('chat message', msg);
+        })
+        
     });
 });
 
